c76026586ddd29dee5abd7351143cf99c5aa4281
Jean Soto <jeancasoto@unitec.edu>
2019-02-13 18:12:01 -0600

    nuevo logo

---
+++ guacamole/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <img src= "http://paizinho.com.br/wp-content/plugins/poptin/assets/images/parrot.gif" alt="logo" />
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
