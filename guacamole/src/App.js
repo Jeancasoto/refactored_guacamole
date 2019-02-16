727fd591fb12936f9d920eab4090b52cfcd3608b
Jean Soto <jeancasoto@unitec.edu>
2019-02-16 15:59:04 -0600

    Login skull

--- guacamole/src/App.js
+++ guacamole/src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import parrot from './Images/parrot.gif';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <header className="App-header">
+          <h1 className="brush_font"> PETTER </h1>
+          <img className="first_page_gif" src= {parrot} alt="first page parrot" />
+          <label className="login_register">Username
+          <input type="text"></input>
+          </label>
 
-          <img src= "http://paizinho.com.br/wp-content/plugins/poptin/assets/images/parrot.gif" alt="logo" />
+          <label className="login_register">Password     
+          <input type="text"></input>
+          
+          </label>
+
+
+          <button className="login_button">LOG IN</button>
+
+          <label className="login_create_account">Don't have account? Create on here!</label>
           
         </header>
+
+        
+
+       
       </div>
     );
   }
