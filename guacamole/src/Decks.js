24dd845c694dc60ee87cf330d64d8e134480f5f7
Jean Soto <jeancasoto@unitec.edu>
2019-02-18 21:20:15 -0600

    agregando imagenes a las cards

--- guacamole/src/Decks.js
+++ guacamole/src/Decks.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import { Card, Button, CardImg, CardTitle, CardText, CardDeck, CardColumns, CardRows,
  CardSubtitle, CardBody } from 'reactstrap';
-import parrot from './Images/parrot.gif';
+import Conejo1 from './Images/conejo1.jpg';
+import Conejo2 from './Images/conejo2.jpg';
+import Gato1 from './Images/gato1.jpg';
+import Pajaro1 from './Images/pajaro1.jpg';
+import Pajaro2 from './Images/pajaro2.jpg';
+import Pajaro3 from './Images/pajaro3.jpg';
+import Pajaro4 from './Images/pajaro4.jpg';
+import Pajaro5 from './Images/pajaro5.jpg';
+import Perro1 from './Images/perro1.jpg';
+import Perro2 from './Images/perro2.jpg';
+import Sphynx from './Images/sphynx.jpg';
+import Perro3 from './Images/perro3.jpg';
+import Perro4 from './Images/perro4.jpg';
+import Perro5 from './Images/perro5.jpg';
+import Perro6 from './Images/perro6.jpg';
+import Perro7 from './Images/perro7.jpg';
+import Perro8 from './Images/perro8.jpg';
+import Perro9 from './Images/perro9.jpg';
+import Perro10 from './Images/perro10.jpg';
+import Husky from './Images/husky.jpg';
+
+
+
 
 
 const Example = (props) => {
@@ -9,7 +31,7 @@ const Example = (props) => {
 
     <CardColumns>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg className="first_page_gif" top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg className="first_page_gif" top width="50%" src={Husky} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -18,7 +40,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Perro1} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -27,7 +49,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Pajaro1} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -36,7 +58,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Conejo1} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -45,7 +67,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Sphynx} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -54,7 +76,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Pajaro3} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -63,7 +85,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Perro2} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -72,7 +94,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Pajaro5} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -81,7 +103,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Pajaro4} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -90,7 +112,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Perro3} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -99,7 +121,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Conejo2} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -108,7 +130,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card style={{width:"80%", height:"15%"}}>
-        <CardImg top width="50%" src={parrot} alt="Card image cap" />
+        <CardImg top width="50%" src={Perro4} alt="Card image cap" />
         <CardBody>
           <CardTitle>Tommy</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
@@ -116,7 +138,78 @@ const Example = (props) => {
           <Button>Button</Button>
         </CardBody>
       </Card>
-       
+      <Card style={{width:"80%", height:"15%"}}>
+        <CardImg top width="50%" src={Perro5} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Tommy</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card style={{width:"80%", height:"15%"}}>
+        <CardImg top width="50%" src={Perro6} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Tommy</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card style={{width:"80%", height:"15%"}}>
+        <CardImg top width="50%" src={Perro7} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Tommy</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card style={{width:"80%", height:"15%"}}>
+        <CardImg top width="50%" src={Perro8} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Tommy</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card style={{width:"80%", height:"15%"}}>
+        <CardImg top width="50%" src={Perro9} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Tommy</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card style={{width:"80%", height:"15%"}}>
+        <CardImg top width="50%" src={Perro10} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Tommy</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card style={{width:"80%", height:"15%"}}>
+        <CardImg top width="50%" src={Pajaro2} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Tommy</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card style={{width:"80%", height:"15%"}}>
+        <CardImg top width="50%" src={Gato1} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Tommy</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>This is a wider card.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card> 
     </CardColumns>
 
     
